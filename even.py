c0089968756edb4ec274c96a4ba00f1d07592351
Uday kumar V <udaykumarv.222@gmail.com>
2026-01-02 11:36:18 +0530

    Add files via upload

---
+++ even.py
@@ -0,0 +1,10 @@
+# to know a given number is even or odd
+
+x=int(input("select a number if you want to know even or odd : " ))
+
+if x%2==0:
+    print("your seleted number is even")
+else :
+    print("your selected number is odd")
+
+print("thanks for useing uday's softwares ")
